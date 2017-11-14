5c6c4a936cb8a1a8816164be1b1bdcea3c33e6ac
Sam Chow <samchowa@gmail.com>
2017-11-14 15:53:50 -0800

    look at all the functions params

--- src/sema.js
+++ src/sema.js
@@ -2,11 +2,28 @@ const bablyon = require('babylon');
 const fs = require('fs');
 
 function main() {
+    // 1. Read the file using the built-in library method fs.readFileSync() called with F
     const sampleFile = fs.readFileSync('./src/simpleProgram.js', 'utf-8');
-    console.log(sampleFile);
+
+
+    // 2. Use the Babylon package’s function `babylon.parse()` on the file,
+    // returning the Abstract Syntax Tree assigned to the variable `A` (ast)
     const ast = bablyon.parse(sampleFile);
 
-    console.log(ast);
+    const body = ast.program.body;
+    // For each Node in the file, do the following:
+    for (var i = 0; i < body.length; i++) {
+        const node = body[i];
+        // Examine the node to see if it is a Function Declaration.
+        // If so, let `ids` be all named parameters to the function:
+        if (node.type === "FunctionDeclaration") {
+            const ids = node.params.map(param => param.name);
+            console.log(ids);
+        }
+    }
+
+
+
 }
 
 
--- src/simpleProgram.js
+++ src/simpleProgram.js
@@ -3,3 +3,8 @@ function sideEffect(modifies, leavesAlone) {
     modifies.x = 12;
     return leavesAlone;
 }
+
+function noEffects(noModify, alsoNoModify) {
+    const me = 12;
+    return me;
+}
