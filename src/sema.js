0db4fbbf85fa00f5dabecb3db38f33b396cfc17a
Sam C <samchowa@gmail.com>
2017-11-15 22:37:41 -0800

    some cleanup with the acc

--- src/sema.js
+++ src/sema.js
@@ -6,7 +6,7 @@ const nodesWithBodies = [
     'BlockStatement',
     'WithStatement',
     'LabeledStatement',
-]
+];
 
 function main() {
     // 1. Read the file using the built-in library method fs.readFileSync() called with F
@@ -31,10 +31,10 @@ function processFunction(functionNode) {
     // If so, let `ids` be all named parameters to the function:
     const { params } = functionNode;
     const ids = params.map(param => param.name);
-    // console.log(' The function node: ', functionNode, '\n');
 
-    // side note: here we have to go into the function's block statement, as it contains everything:
     const allNodes = pullAllStatements([functionNode.body]);
+
+    let sideEffectsAccumulator = [];
 
     // Examine each Statement in the Function Declaration Body, and for each:
     for (let i = 0; i < allNodes.length; i++) {
@@ -45,20 +45,22 @@ function processFunction(functionNode) {
         if (node.type === "AssignmentExpression") {
             // Examine the left hand side of the Assignment Expression, and if the result is an identifier, or a
             const { left, right } = node;
+
             // Member Expression with the object property being an Identifier contained within IDS:
             if (left.type === 'MemberExpression') {
-                // console.log('left', left, '\n');
 
                 if (left.object.type === 'Identifier' && ids.includes(left.object.name)) {
-                    console.error('\n\n!!!Found a side effect node!');
-                    console.log('Line ', left.loc.start.line, 'contains a side effect, with the identifier', left.object.name, '\n');
+                    // console.error('\n\n!!!Found a side effect node!');
+                    // console.log('Line ', left.loc.start.line, 'contains a side effect, with the identifier', left.object.name, '\n');
+                    sideEffectsAccumulator = sideEffectsAccumulator.concat(
+                        `Line ${left.loc.start.line} contains a side effect, namely modifing the identifier ${left.object.name}, which is bound in the function named ${functionNode.id.name}\n`
+                    );
                 }
             }
         }
     }
 
-
-    console.log('\n\n all nodes we found: ', allNodes);
+    console.error(sideEffectsAccumulator);
 }
 
 
@@ -98,11 +100,15 @@ function pullAllStatements(nodesToInspect, allResults = []) {
         const nodesToAdd = [
             node.discriminant,
             // add all the test cases, but also remove the default (which will be null for test)
-            ...node.cases.map(switchCase => switchCase.test).filter(i => i),
-            ...node.cases.map(switchCase => switchCase.consequent),
+            ...(node.cases.map(switchCase => switchCase.test).filter(i => i)),
+            ...(node.cases.map(switchCase => switchCase.consequent)),
         ];
 
         return pullAllStatements(restNodes.concat(...nodesToAdd), allResults);
+    }
+
+    if (node.type === 'ExpressionStatement') {
+        return pullAllStatements(restNodes.concat(node.expression), allResults);
     }
 
     // otherwise this node is good and doesn't have subsequent bodies, so we'll just add it to the results!
